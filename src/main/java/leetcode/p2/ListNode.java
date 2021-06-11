30b71ddb28e2db37f254f8686133bdf52359ea7a
zhangzh <zhangzh@ffcs.cn>
2021-06-11 13:58:15 +0800

    feat: 第2题解法

---
+++ src/main/java/leetcode/p2/ListNode.java
@@ -0,0 +1,24 @@
+package leetcode.p2;
+
+/**
+ *
+ * @author zhangzh
+ * @date 2021/06/11
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
